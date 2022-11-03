8014470427a79c902bc50959a904cc4d98ba7c4e
sebhz <seb@zyglute.fr>
2022-11-03 20:42:18 +0100

    Fixes to sun monitor

--- components/sun.c
+++ components/sun.c
@@ -13,13 +13,13 @@ get_sun_coord (double phi, double L, double *A, double *h)
     dt_get_current_jd (0, &jd);
     jde = dy_ut_to_dt (jd);
     /* Get sun apparent coordinates. Low accuracy is more than enough */
-    sun_apparent_geocentric_coord (jde, &alpha, &delta, 0);
+    sun_apparent_equatorial_coord (jde, &alpha, &delta, 0);
 
     /* We now have the sun geocentric coordinates.
        Let's convert them to horizontal coordinates
        at the observer's latitude and longitude */
     coo_get_local_hour_angle (jd, L, alpha, &H, 1);
-    coo_ecl_to_hor (H, delta, phi, A, h);
+    coo_equ_to_hor (H, delta, phi, A, h);
     /* and correct altitude for the atmospheric refraction
        as we want to get the "measured" coordinates */
     *h += ref_refraction_true_to_apparent (*h, 1) / 60;
